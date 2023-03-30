05c908e4a8242dfe03be9204ff698089805ae5b4
BenCodHedges <benjamin.hedges@codurance.com>
2023-03-30 16:36:20 +0100

    added get and post requests for Health Check and basic subscriptions post

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
 
-    println!("Hello, world!");
+#[tokio::main]
+// #[actix_web::main]
+async fn main() -> Result<(), std::io::Error>{
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let _ = run(listener);
+    Ok(())
 }
